838ec3d4f63ed75fd1644d8c54bc47a25791387a
Kari-Kranich <114507142+Kari-Kranich@users.noreply.github.com>
2022-10-17 11:10:59 -0700

    Lab 5 Update

---
+++ lab5/js/lab.js
@@ -0,0 +1,24 @@
+/*
+* Author: Kari Kranich <kkranich@ucsc.edu>
+* Created: 17 October
+* License: Public DOman
+*/
+
+// Define Variables
+make = "chrysler";
+model = "PT Cruiser";
+color = "Gray";
+year = 2001;
+
+
+// Calculate
+
+age = 2022 - year;
+
+// Output
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
