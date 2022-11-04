c63872638764769e5aafe23b9162e77d4e06ee38
Kari-Kranich <114507142+Kari-Kranich@users.noreply.github.com>
2022-11-04 09:04:06 -0700

    lab11

---
+++ lab11/js/lab.js
@@ -0,0 +1,31 @@
+/* Author: Kari Kranich
+* Created: Nov 1
+* License: Public Domain
+*/
+// VV button  . click to start function
+$("#c_button").click(function () {
+    // VV div id you would like to change
+    //                              VV class you would like to toggle
+    $("#challenges").toggleClass("special");
+    $("#c_button").toggleClass("special");
+
+});
+
+$("#p_button").click(function () {
+    $("#problems").toggleClass("special");
+    $("#p_button").toggleClass("special");
+
+});
+
+$("#r_button").click(function () {
+    $("#results").toggleClass("special");
+    $("#r_button").toggleClass("special");
+
+});
+
+$("#o_button").click(function () {
+    $("#output").toggleClass("special");
+    $("#o_button").toggleClass("special");
+
+});
+}
